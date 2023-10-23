b3ae32f862998651b35096cf66b89e59a1bbc398
dreadlordhar <148783968+dreadlordhar@users.noreply.github.com>
2023-10-23 15:24:20 +0000

    Update DisplayThings.pas

--- src/DisplayThings.pas
+++ src/DisplayThings.pas
@@ -78,29 +78,6 @@ begin
 	end;
 	
 	for i := 1 to ScreenHeight dounit SnakeConstants;
-
-interface
-const
-	snakebodychar = '#';
-	delayTime = 70; {0.07 second}
-
-type
-	snake = ^snakein;
-	snakein = record
-		x: integer;
-		y: integer;
-		next: snake;
-	end;
-
-	faceSnake = (up, down, right, left);
-	
-	gapple = record
-		x: integer;
-		y: integer;
-	end;
-implementation
-begin end.
-
 	begin
 		GotoXY(ScreenWidth, i);
 		write('|');
